c0962de7d8b2b64a34303739104012b2547579c5
wernerr <rene.werner@verivox.com>
2020-05-30 00:18:31 +0200

    fix race condition

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ package logger
 import (
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 
 	"github.com/gofiber/fiber"
@@ -25,7 +26,7 @@ func TestNew_withRoutePath(t *testing.T) {
 		Format: format,
 		Output: buf,
 	})
-	app := fiber.New()
+	app := fiber.New(&fiber.Settings{DisableStartupMessage: true})
 	app.Use(n)
 
 	app.Get(routePath, func(ctx *fiber.Ctx) {
@@ -41,5 +42,32 @@ func TestNew_withRoutePath(t *testing.T) {
 
 	if buf.String() != expectedOutput {
 		t.Errorf("Has: %s, expected: %s", buf.String(), expectedOutput)
+	}
+}
+
+func TestNew_withDefaultFormat(t *testing.T) {
+	expectedOutputPattern := regexp.MustCompile(`^\d{2}:\d{2}:\d{2} GET / - 0\.0\.0\.0 - 200 - \d\.\d+.{1,3}
+$`)
+	// fake output
+	buf := bytebufferpool.Get()
+	defer bytebufferpool.Put(buf)
+
+	n := New(Config{Output: buf})
+	app := fiber.New(&fiber.Settings{DisableStartupMessage: true})
+	app.Use(n)
+
+	app.Get("/", func(ctx *fiber.Ctx) {
+		ctx.SendStatus(200)
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	_, err := app.Test(req, 1000)
+	if err != nil {
+		t.Errorf("Has: %+v, expected: nil", err)
+	}
+
+	if false == expectedOutputPattern.MatchString(buf.String()) {
+		t.Errorf("Has: %s, expected pattern: %s", buf.String(), expectedOutputPattern.String())
 	}
 }
